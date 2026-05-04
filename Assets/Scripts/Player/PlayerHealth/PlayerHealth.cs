6df467bc41e1e9ba3f97395e15e12a74063cd17c
grosau <olof.andreasson06@gmail.com>
2026-05-04 17:00:15 +0200

    added playerHealth

---
+++ Assets/Scripts/Player/PlayerHealth/PlayerHealth.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public delegate void OnPlayerDeath();
+    public static event OnPlayerDeath _onPlayerDeath;
+
+    public delegate void OnHealthChange();
+    public static event OnHealthChange _onHealthChange;
+
+    [SerializeField] float maxHealth;
+    [SerializeField] float currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        _onHealthChange?.Invoke();
+        if (currentHealth <= 0)
+        {
+            _onPlayerDeath?.Invoke();
+        }
+
+    }
+}
