92a5f00c19fd1585a90d3033661ebdbf460d312f
grosau <olof.andreasson06@gmail.com>
2026-08-02 16:45:44 +0200

    Game Manager made

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public enum GameState
+    {
+        MainMenu,
+        Corridor,
+        Arena,
+        PowerUpSelection,
+        GameOver
+    }
+
+    public GameState currentGameState;
+
+    public static GameManager Instance;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void Start()
+    {
+        ChangeState(GameState.MainMenu);
+    }
+
+    public delegate void GameStateChanged(GameState newState);
+    public static event GameStateChanged OnGameStateChanged;
+
+    public void ChangeState(GameState newState)
+    {
+        currentGameState = newState;
+        OnGameStateChanged?.Invoke(newState);
+    }
+}
