8bc6cdfa7d7d99d690b17ed4b5ae1eda74eea88f
grosau <olof.andreasson06@gmail.com>
2026-08-24 17:24:26 +0200

    added a notes scripts

---
+++ Assets/Scripts/Notes.cs
@@ -0,0 +1,11 @@
+// NOTES
+// main menu sohuld have, play, settings, quit game
+// corridor should have, health and a sanity meter and a  horizontal "list" of icons with the selected powerups in the top of the screen like in risk of rain 2. and a counter for the bullets in the current magazine and a seperate counter for the bullets left in unsused magazines
+
+// the arena should have the same ui elements as corridor
+
+// in the powerup selection should be the same as arena ad corridor but with the 3 different powerups overlayed in ontop, and the camera should be locked and the mouse visible and free to move around the screen
+
+// in the game over all of the ui should dissapear, and the only thing should be a menu with some different death quotes and buttons to go back to the main menu and retry. I also want to add  screen distortion and data moshing before you die
+
+// POWERUPS
